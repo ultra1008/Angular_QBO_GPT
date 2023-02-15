f141b36596970b03be0d57817b0ffbbc44df8737
ultra1008 <ultra921008@gmail.com>
2023-02-15 06:26:46 -0500

    Update attachment API for Invoice

--- invoice-backend/model/history/invoice_history.js
+++ invoice-backend/model/history/invoice_history.js
@@ -110,7 +110,7 @@ var invoice_history_Schema = new Schema({
 
     history_created_at: { type: Number, default: 0 },
     history_created_by: { type: mongoose.ObjectId, default: "" },
-    action: { type: String, enum: ["Insert", "Update", "Delete", "Insert Note", "Update Note", "Delete Note"] },
+    action: { type: String, enum: ["Insert", "Update", "Delete", "Insert Note", "Update Note", "Delete Note", "Update Attachment"] },
     taken_device: { type: String, default: "Web", enum: ["Mobile", "Web"] },
     invoice_id: { type: mongoose.ObjectId, default: "" }
 });
