d9fb4fbee74ba1b09621734bef0bcc6fa5bf40a1
ultra1008 <ultra921008@gmail.com>
2023-03-15 05:57:03 -0400

    invoice report title set.

--- invoice-frontend/src/app/pages/components/invoice/invoice.component.ts
+++ invoice-frontend/src/app/pages/components/invoice/invoice.component.ts
@@ -94,7 +94,7 @@ export class InvoiceComponent implements OnInit {
   showInvoiceTable = true;
   dtOptions: DataTables.Settings = {};
 
-  constructor (private router: Router, private modeService: ModeDetectService, public mostusedservice: Mostusedservice,
+  constructor(private router: Router, private modeService: ModeDetectService, public mostusedservice: Mostusedservice,
     public translate: TranslateService, public dialog: MatDialog,
     public httpCall: HttpCall, public snackbarservice: Snackbarservice, public uiSpinner: UiSpinnerService) {
     let tmp_gridtolist_team = localStorage.getItem("gridtolist_invoice_list");
@@ -317,7 +317,7 @@ export class InvoiceComponent implements OnInit {
     const dialogRef = this.dialog.open(InvoiceReport, {
       height: '500px',
       width: '800px',
-      data: { vendorList: this.vendorsList },
+      data: { vendorList: this.vendorsList, },
       disableClose: true
     });
 
@@ -444,7 +444,7 @@ export class InvoiceAttachment {
   FILE_NOT_SUPPORTED: string;
   Invoice_Add_Atleast_One_Document: string = '';
 
-  constructor (private modeService: ModeDetectService, private formBuilder: FormBuilder, public httpCall: HttpCall,
+  constructor(private modeService: ModeDetectService, private formBuilder: FormBuilder, public httpCall: HttpCall,
     public dialogRef: MatDialogRef<InvoiceAttachment>,
     @Inject(MAT_DIALOG_DATA) public data: any, public sb: Snackbarservice, public translate: TranslateService, public dialog: MatDialog, private sanitiser: DomSanitizer,
     public snackbarservice: Snackbarservice, public uiSpinner: UiSpinnerService,
@@ -786,7 +786,7 @@ export class InvoiceReport {
   copyDataFromProject: string = '';
   add_my_self_icon = icon.ADD_MY_SELF_WHITE;
 
-  constructor (private modeService: ModeDetectService, private formBuilder: FormBuilder, public httpCall: HttpCall,
+  constructor(private modeService: ModeDetectService, private formBuilder: FormBuilder, public httpCall: HttpCall,
     public dialogRef: MatDialogRef<InvoiceReport>,
     @Inject(MAT_DIALOG_DATA) public data: any, public sb: Snackbarservice, public translate: TranslateService) {
 
