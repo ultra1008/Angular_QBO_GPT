3ef9a7d278da09125d897e7a69f6815bd14da46c
ultra1008 <ultra921008@gmail.com>
2023-04-10 06:58:11 -0400

    unused wasabiImagePath removed

--- invoice-frontend/src/app/consts/wasabiImagePath.ts
+++ invoice-frontend/src/app/consts/wasabiImagePath.ts
@@ -1,31 +1,11 @@
 export enum wasabiImagePath {
-    BANK_DETAIL_VOIDED_CHEQUE = "bank_details/voided_cheque/",
-    BANK_DETAIL_SIGNATURE = "bank_details/voided_cheque/",
-    VENDOR_PROFILE = "vendor/profile/",
     VENDOR_CERTIFICATES = "vendor/certificates/",
-    EMAIL_TEMPLATE_LOGO = "email_template_logo/",
     PROJECT_LOGO = "project/logo/",
     PROJECT_DOCUMENT = "project/documents/",
-    VENDOR_PERFORMANCE_SIGNATURE = "vendor_performances/signature",
-    VENDOR_PERFORMANCE_ATTACHEMENTS = "vendor_performances/attachments",
-    VENDOR_PERFORMANCE_THUMBNAIL_ATTACHEMENTS = "vendor_performances/thumbnail_attachments",
-    CHANGE_ORDER_SIGNATURE = "change_order/signature",
-    CHANGE_ORDER_ATTACHEMENTS = "change_order/attachments",
-    CHANGE_ORDER_THUMBNAIL_ATTACHEMENTS = "change_order/thumbnail_attachments",
-    OWNER_DIRECT_PURCHASE_SIGNATURE = "owner_direct_purchase/signature",
-    OWNER_DIRECT_PURCHASE_ATTACHEMENTS = "owner_direct_purchase/attachments",
-    OWNER_DIRECT_PURCHASE_THUMBNAIL_ATTACHEMENTS = "owner_direct_purchase/thumbnail_attachments",
-    VENDOR_PROJECT_PAYMENT_SIGNATURE = "payment/signature",
-    VENDOR_PROJECT_PAYMENT_ATTACHEMENTS = "payment/attachments",
-    VENDOR_PROJECT_PAYMENT_THUMBNAIL_ATTACHEMENTS = "payment/thumbnail_attachments",
-    VENDOR_CERTIFICATE_DOCUMENT_ATTACHEMENTS = "vendor_certificate/attachments",
-    CONTRACT_DOCUMENT_ATTACHEMENTS = "contract_documents/attachments",
     INVOICE_ATTCHMENT = "invoice_attchments/attchments",
     Quote_attachments = "quote_attachments/attachments",
     Po_attachments = "po_attachments/attachments",
     Packing_slip_attachments = "packing_slip_attachments/attachments",
     Reciving_slip_attachments = "Reciving_slip_attachments/attachments",
-
-
     VENDOR_ATTECHMENT = 'vendor'
 }
