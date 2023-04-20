e0b9046c41c2d873808948dbbd54c3f470bdc01d
ultra1008 <ultra921008@gmail.com>
2023-04-20 01:05:58 -0400

    all  Alerts pop up add branding .

--- invoice-frontend/src/app/service/snack-bar-service.ts
+++ invoice-frontend/src/app/service/snack-bar-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 @Injectable({ providedIn: 'root' })
 export class Snackbarservice {
@@ -13,33 +13,39 @@ export class Snackbarservice {
             showConfirmButton: false,
             timer: 3000,
             timerProgressBar: true,
+            // imageUrl: './assets/logo/invoice_logo.png',
+            // imageHeight: 50,
+            // imageAlt: 'A branding image',
             didOpen: (toast) => {
-                toast.addEventListener('mouseenter', Swal.stopTimer)
-                toast.addEventListener('mouseleave', Swal.resumeTimer)
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
             }
-        })
+        });
+        message = `<div class="d-flex justify-content-between"><span>${message}</span> <span class="m-auto"><img src='./assets/logo/invoice_logo.png' height='40px'/></span></div>`;
         if (action == 'success') {
             Toast.fire({
+                width: 400,
                 icon: action,
-                title: message
-            })
+                title: message,
+            });
         } else if (action == 'warning') {
             Toast.fire({
+                width: 400,
                 icon: action,
                 title: message
-            })
-        }
-        else if (action == 'info') {
+            });
+        } else if (action == 'info') {
             Toast.fire({
+                width: 400,
                 icon: action,
                 title: message
-            })
-        }
-        else {
+            });
+        } else {
             Toast.fire({
+                width: 400,
                 icon: "error",
                 title: message
-            })
+            });
         }
     }
 
