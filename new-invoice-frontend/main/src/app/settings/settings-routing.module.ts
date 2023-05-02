5619183fade1a775c9bea0be39afb0ccbde0b696
ultra1008 <ultra921008@gmail.com>
2023-05-02 06:51:07 -0400

    Settings improvement added

--- new-invoice-frontend/main/src/app/settings/settings-routing.module.ts
+++ new-invoice-frontend/main/src/app/settings/settings-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllsettingsComponent } from './allsettings/allsettings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AllsettingsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
