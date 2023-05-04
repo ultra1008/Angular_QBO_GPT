2e91df5c0c9ef51fa5ccdb6dbef3042e555b14e4
ultra1008 <ultra921008@gmail.com>
2023-05-04 02:26:42 -0400

    Add Screen loader and Datatable loader

--- new-invoice-frontend/main/src/app/common-components/common-components.module.ts
+++ new-invoice-frontend/main/src/app/common-components/common-components.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoryComponent } from './history/history.component';
 import { HistoryDateTimePipe } from './common.pipe';
+import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
   declarations: [
     HistoryComponent,
-    HistoryDateTimePipe
+    HistoryDateTimePipe,
+    LoadingComponent,
   ],
   exports: [
-    HistoryComponent
+    HistoryComponent,
+    LoadingComponent,
   ],
   imports: [
     CommonModule,
