ee4a3066f7c7826e4ca7a7608ba385fad9b20268
ultra1008 <ultra921008@gmail.com>
2023-05-11 23:26:41 -0400

    Document module side menu routing set

--- new-invoice-frontend/main/src/app/documents/documents-routing.module.ts
+++ new-invoice-frontend/main/src/app/documents/documents-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentsListingComponent } from './documents-listing/documents-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DocumentsListingComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- new-invoice-frontend/main/src/app/documents/documents.module.ts
+++ new-invoice-frontend/main/src/app/documents/documents.module.ts
@@ -3,15 +3,40 @@ import { CommonModule } from '@angular/common';
 
 import { DocumentsRoutingModule } from './documents-routing.module';
 import { DocumentsListingComponent } from './documents-listing/documents-listing.component';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { CommonComponentsModule } from '../common-components/common-components.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SettingsService } from '../settings/settings.service';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
     DocumentsListingComponent
   ],
+  providers: [SettingsService],
   imports: [
     CommonModule,
-    DocumentsRoutingModule
+    DocumentsRoutingModule,
+    TranslateModule,
+    MatTabsModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatPaginatorModule,
+    CommonComponentsModule, // App Loading module
+    MatTooltipModule, // Tooltip Module
+    MatDialogModule,
+    MatSortModule, // Datatable sorting module
+    MatSelectModule // Datatable filter module
   ]
 })
 export class DocumentsModule { }
