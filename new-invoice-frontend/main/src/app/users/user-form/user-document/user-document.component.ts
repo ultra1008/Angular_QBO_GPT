c1e884f5997a4cf7206875150e751c816a150b10
ultra1008 <ultra921008@gmail.com>
2023-05-22 02:48:28 -0400

    User Save and Update with picture

---
+++ new-invoice-frontend/main/src/app/users/user-form/user-document/user-document.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-document',
+  templateUrl: './user-document.component.html',
+  styleUrls: ['./user-document.component.scss']
+})
+export class UserDocumentComponent {
+
+}
---
+++ new-invoice-frontend/main/src/app/users/user-form/user-emergency-contact/user-emergency-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-emergency-contact',
+  templateUrl: './user-emergency-contact.component.html',
+  styleUrls: ['./user-emergency-contact.component.scss']
+})
+export class UserEmergencyContactComponent {
+
+}
