54ab5e0dff18f0443693370ce66105ba1856cf18
ultra1008 <ultra921008@gmail.com>
2023-05-27 05:13:06 -0400

    other document add pdf viwer

--- new-invoice-frontend/main/src/app/invoice/view-document/view-document.component.ts
+++ new-invoice-frontend/main/src/app/invoice/view-document/view-document.component.ts
@@ -26,7 +26,7 @@ export class ViewDocumentComponent {
   document: any;
   documentTypes = configData.DOCUMENT_TYPES;
 
-  constructor (private fb: UntypedFormBuilder, public commonService: CommonService, public route: ActivatedRoute) {
+  constructor(private fb: UntypedFormBuilder, public commonService: CommonService, public route: ActivatedRoute) {
     this.document = this.route.snapshot.queryParamMap.get('document') ?? '';
 
     this.poForm = this.fb.group({
@@ -85,6 +85,45 @@ export class ViewDocumentComponent {
     if (data.status) {
       this.variablestermList = data.data;
       this.termsList = this.variablestermList.slice();
+    }
+  }
+  print() {
+    fetch(this.pdf_url).then(resp => resp.arrayBuffer()).then(resp => {
+      /*--- set the blog type to final pdf ---*/
+      const file = new Blob([resp], { type: 'application/pdf' });
+      const blobUrl = window.URL.createObjectURL(file);
+      const iframe = document.createElement('iframe');
+      iframe.style.display = 'none';
+      iframe.src = blobUrl;
+      document.body.appendChild(iframe);
+      //iframe.contentWindow.print();
+      iframe.onload = () => {
+        setTimeout(() => {
+          iframe.focus();
+          iframe.contentWindow!.print();
+        });
+      };
+    });
+  }
+
+  download() {
+    let a = document.createElement('a');
+    /*--- Firefox requires the link to be in the body --*/
+    document.body.appendChild(a);
+    a.style.display = 'none';
+    a.target = "_blank";
+    a.href = this.pdf_url;
+    a.click();
+    /*--- Remove the link when done ---*/
+    document.body.removeChild(a);
+  }
+  onKey(event: any) {
+
+    if (event.target.value.length == 0) {
+
+      // this.dashboardHistory = [];
+      // this.start = 0;
+      // this.getTodaysActivity();
     }
   }
 }
