57913c8ae6fecb4faf70364e9ef174aa4569c17b
ultra1008 <ultra921008@gmail.com>
2023-05-29 23:33:59 -0400

    add sorting on document type

--- invoice-backend/model/document_type.js
+++ invoice-backend/model/document_type.js
@@ -4,6 +4,8 @@ var documenttypeSchema = new Schema({
     document_type_name: { type: String },
     is_expiration: { type: Boolean, default: false },
     is_delete: { type: Number, default: 0 },
+    created_at: { type: Number },
+    updated_at: { type: Number },
 }, { collection: 'document_type' });
 
 module.exports = documenttypeSchema;
