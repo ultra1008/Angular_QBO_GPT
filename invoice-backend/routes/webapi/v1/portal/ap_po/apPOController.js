3ef23e037ba2a939dce106f3c9837e788f3880cc
ultra1008 <ultra921008@gmail.com>
2023-06-05 22:34:46 -0400

    Make Get and Save API for Supporting documents i.e. PO, Quote, Packing Slip, Receiving Slip

---
+++ invoice-backend/routes/webapi/v1/portal/ap_po/apPOController.js
@@ -0,0 +1,90 @@
+var apPOSchema = require('./../../../../../model/ap_pos');
+let db_connection = require('./../../../../../controller/common/connectiondb');
+let collectionConstant = require('./../../../../../config/collectionConstant');
+let common = require('./../../../../../controller/common/common');
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports.getAPPO = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var apPOConnection = connection_db_api.model(collectionConstant.AP_PO, apPOSchema);
+            var get_data = await apPOConnection.find({ is_delete: 0 });
+            res.send({ status: true, message: "PO Listing", data: get_data });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), error: e, status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
+
+module.exports.getOneAPPO = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var requestObject = req.body;
+            var apPOConnection = connection_db_api.model(collectionConstant.AP_PO, apPOSchema);
+            var get_data = await apPOConnection.findOne({ _id: ObjectID(requestObject._id) });
+            res.send({ status: true, message: "PO Listing", data: get_data });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), error: e, status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
+
+module.exports.saveAPPO = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    var local_offset = Number(req.headers.local_offset);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var requestObject = req.body;
+            var apPOConnection = connection_db_api.model(collectionConstant.AP_PO, apPOSchema);
+            var id = requestObject._id;
+            delete requestObject._id;
+            if (id) {
+                requestObject.updated_at = Math.round(new Date().getTime() / 1000);
+                requestObject.updated_by = decodedToken.UserData._id;
+                let update_ap_packing_slip = await apPOConnection.updateOne({ _id: ObjectID(id) }, requestObject);
+                if (update_ap_packing_slip) {
+                    res.send({ status: true, message: "PO updated successfully.", data: update_ap_invoice });
+                } else {
+                    res.send({ message: translator.getStr('SomethingWrong'), status: false });
+                }
+            } else {
+                requestObject.created_at = Math.round(new Date().getTime() / 1000);
+                requestObject.created_by = decodedToken.UserData._id;
+                requestObject.updated_at = Math.round(new Date().getTime() / 1000);
+                requestObject.updated_by = decodedToken.UserData._id;
+                let add_ap_packing_slip = new apPOConnection(requestObject);
+                let save_ap_packing_slip = await add_ap_packing_slip.save();
+                if (save_ap_packing_slip) {
+                    res.send({ status: true, message: "PO added successfully.", data: save_ap_packing_slip });
+                } else {
+                    res.send({ message: translator.getStr('SomethingWrong'), status: false });
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
---
+++ invoice-backend/routes/webapi/v1/portal/ap_receiving_slip/apReceivingSlipController.js
@@ -0,0 +1,90 @@
+var apReceivingSlipSchema = require('./../../../../../model/ap_receivingslips');
+let db_connection = require('./../../../../../controller/common/connectiondb');
+let collectionConstant = require('./../../../../../config/collectionConstant');
+let common = require('./../../../../../controller/common/common');
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports.getAPReceivingSlip = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var apReceivingSlipConnection = connection_db_api.model(collectionConstant.AP_RECEIVING_SLIP, apReceivingSlipSchema);
+            var get_data = await apReceivingSlipConnection.find({ is_delete: 0 });
+            res.send({ status: true, message: "Receiving Slip Listing", data: get_data });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), error: e, status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
+
+module.exports.getOneAPReceivingSlip = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var requestObject = req.body;
+            var apReceivingSlipConnection = connection_db_api.model(collectionConstant.AP_RECEIVING_SLIP, apReceivingSlipSchema);
+            var get_data = await apReceivingSlipConnection.findOne({ _id: ObjectID(requestObject._id) });
+            res.send({ status: true, message: "Receiving Slip Listing", data: get_data });
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), error: e, status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
+
+module.exports.saveAPReceivingSlip = async function (req, res) {
+    var decodedToken = common.decodedJWT(req.headers.authorization);
+    var translator = new common.Language(req.headers.Language);
+    var local_offset = Number(req.headers.local_offset);
+    if (decodedToken) {
+        let connection_db_api = await db_connection.connection_db_api(decodedToken);
+        try {
+            var requestObject = req.body;
+            var apReceivingSlipConnection = connection_db_api.model(collectionConstant.AP_RECEIVING_SLIP, apReceivingSlipSchema);
+            var id = requestObject._id;
+            delete requestObject._id;
+            if (id) {
+                requestObject.updated_at = Math.round(new Date().getTime() / 1000);
+                requestObject.updated_by = decodedToken.UserData._id;
+                let update_ap_receiving_slip = await apReceivingSlipConnection.updateOne({ _id: ObjectID(id) }, requestObject);
+                if (update_ap_receiving_slip) {
+                    res.send({ status: true, message: "Receiving Slip updated successfully.", data: update_ap_invoice });
+                } else {
+                    res.send({ message: translator.getStr('SomethingWrong'), status: false });
+                }
+            } else {
+                requestObject.created_at = Math.round(new Date().getTime() / 1000);
+                requestObject.created_by = decodedToken.UserData._id;
+                requestObject.updated_at = Math.round(new Date().getTime() / 1000);
+                requestObject.updated_by = decodedToken.UserData._id;
+                let add_ap_receiving_slip = new apReceivingSlipConnection(requestObject);
+                let save_ap_receiving_slip = await add_ap_receiving_slip.save();
+                if (save_ap_receiving_slip) {
+                    res.send({ status: true, message: "Receiving Slip added successfully.", data: save_ap_receiving_slip });
+                } else {
+                    res.send({ message: translator.getStr('SomethingWrong'), status: false });
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            res.send({ message: translator.getStr('SomethingWrong'), status: false });
+        } finally {
+            connection_db_api.close();
+        }
+    } else {
+        res.send({ status: false, message: translator.getStr('InvalidUser') });
+    }
+};
